14d72108fc254c7c2325b615d8a23189591b64a5
crash <snickerless1@shaw.ca>
2017-12-13 23:05:12 -0700

    updates to day 10 to improve day 14

--- Solutions/Day10.py
+++ Solutions/Day10.py
@@ -67,6 +67,29 @@ def getDenseHash(circleList):
 		denseHash.append(currentByte)
 	return denseHash
 
+# gets the full knot hash from a bytes or bytearray object
+# returns knot hash (bytearray)
+def getKnotHash(bytesToHash):
+	inputBytes = bytearray(bytesToHash)
+	# add predetermined lengths to end of sequence
+	inputBytes.append(17)
+	inputBytes.append(31)
+	inputBytes.append(73)
+	inputBytes.append(47)
+	inputBytes.append(23)
+	# run 64 rounds of knot-tying, remembering currentPos and skipSize
+	circleList = list(range(0, 256))
+	currentPos = 0
+	skipSize = 0
+	for i in range(0, 64):
+		result = tieKnots2(circleList, inputBytes, currentPos, skipSize)
+		circleList = result[0]
+		currentPos = result[1]
+		skipSize = result[2]
+	# get and print hash
+	denseHash = getDenseHash(circleList)
+	return denseHash
+	
 if __name__ == "__main__":
 	# part 1
 	# read input file into list of int
@@ -83,21 +106,6 @@ if __name__ == "__main__":
 	# read input file into bytearray
 	with open('Day10Input.txt', 'r') as myfile:
 		inputBytes = bytearray(myfile.read().strip(), "ascii")
-	# add predetermined lengths to end of sequence
-	inputBytes.append(17)
-	inputBytes.append(31)
-	inputBytes.append(73)
-	inputBytes.append(47)
-	inputBytes.append(23)
-	# run 64 rounds of knot-tying, remembering currentPos and skipSize
-	currentPos = 0
-	skipSize = 0
-	for i in range(0, 64):
-		result = tieKnots2(circleList, inputBytes, currentPos, skipSize)
-		circleList = result[0]
-		currentPos = result[1]
-		skipSize = result[2]
-	# get and print hash
-	denseHash = getDenseHash(circleList)
-	denseHex = binascii.hexlify(denseHash).decode("ascii")
-	print("Part 2 - Dense Hash: " + denseHex)
+	knotHash = getKnotHash(inputBytes)
+	hashString = binascii.hexlify(knotHash).decode("ascii")
+	print("Part 2 - Dense Hash: " + hashString)
